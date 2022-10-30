a1513a7fbe5a0a42ce9fef36d7c153db8a7d948e
Jean Oldor Saintus <josaintus@gmail.com>
2022-10-30 01:10:05 -0400

    update projet forfaits

--- src/app/forfait/forfait.component.ts
+++ src/app/forfait/forfait.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Avis } from '../avis';
 import { Forfait } from '../forfait';
 
+
 @Component({
   selector: 'app-forfait',
   templateUrl: './forfait.component.html',
@@ -11,7 +12,7 @@ export class ForfaitComponent implements OnInit {
  
   @Input() forfait?: Forfait;
   @Input() avis?: Avis;
-  
+
   constructor() { }
 
   ngOnInit(): void {
