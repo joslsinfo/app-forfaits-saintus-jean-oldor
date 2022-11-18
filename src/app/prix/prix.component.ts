3a48d71ae000497ea263b3d91beeb96141c97ba2
Jean Oldor Saintus <josaintus@gmail.com>
2022-11-17 23:04:22 -0500

    Mise à jour  dans forfaits app

--- src/app/prix/prix.component.ts
+++ src/app/prix/prix.component.ts
@@ -3,6 +3,7 @@ import { Avis } from '../avis';
 import { Etablissement } from '../etablissement';
 import { Forfait } from '../forfait';
 
+
 @Component({
   selector: 'app-prix',
   templateUrl: './prix.component.html',
@@ -13,7 +14,10 @@ export class PrixComponent implements OnInit {
   @Input() forfait?: Forfait;
   @Input() avis?: Avis;
   @Input() etablissment?: Etablissement;
+  
   @Input() prix?: Forfait;
+  @Input() nouveauPrix?: Forfait;
+
 
 
 
