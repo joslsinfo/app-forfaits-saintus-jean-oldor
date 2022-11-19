5c316fa1d605dc1cd3cee08c9f4e83d2df46d2d7
Jean Oldor Saintus <josaintus@gmail.com>
2022-11-18 23:30:59 -0500

    Mise à jour  dans forfaits app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {MatCardModule} from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule} from '@angular/material/card';
+import { MatListModule} from '@angular/material/list';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms'; 
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatSliderModule} from '@angular/material/slider';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { MatSliderModule} from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
