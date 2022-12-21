912d8dcf16d1b85003cee8659d9187d594ee7d6b
Jean Oldor Saintus <josaintus@gmail.com>
2022-12-21 16:38:06 -0500

    Modification de component tableau-forfaits pour ajouter le filter et le tri

--- src/app/tableau-forfaits/tableau-forfaits.component.ts
+++ src/app/tableau-forfaits/tableau-forfaits.component.ts
@@ -1,6 +1,7 @@
 import { FORFAITS } from './../mock-forfaits';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { MatPaginator, } from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Forfait } from '../forfait';
 
@@ -34,7 +35,38 @@ import { Forfait } from '../forfait';
 //   {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
 // ];
 
-
+/** Constants used to fill up our data base. */
+// const FRUITS: string[] = [
+//   'blueberry',
+//   'lychee',
+//   'kiwi',
+//   'mango',
+//   'peach',
+//   'lime',
+//   'pomegranate',
+//   'pineapple',
+// ];
+// const NAMES: string[] = [
+//   'Maia',
+//   'Asher',
+//   'Olivia',
+//   'Atticus',
+//   'Amelia',
+//   'Jack',
+//   'Charlotte',
+//   'Theodore',
+//   'Isla',
+//   'Oliver',
+//   'Isabella',
+//   'Jasper',
+//   'Cora',
+//   'Levi',
+//   'Violet',
+//   'Arthur',
+//   'Mia',
+//   'Thomas',
+//   'Elizabeth',
+// ];
 
 
 
@@ -49,14 +81,48 @@ export class TableauForfaitsComponent implements OnInit {
   dataSource:  MatTableDataSource<Forfait> = new  MatTableDataSource (FORFAITS);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
-  constructor() { }
+  constructor() {
+    // Create 100 users
+    // const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    // this.dataSource = new MatTableDataSource(users);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+
+/** Builds and returns a new User. */
+// function createNewUser(id: number): UserData {
+//   const name =
+//     NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+//     ' ' +
+//     NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+//     '.';
+
+//   return {
+//     id: id.toString(),
+//     name: name,
+//     progress: Math.round(Math.random() * 100).toString(),
+//     fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+//   };
+// }
